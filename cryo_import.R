2358025ce69c55c85795f978d1c613192ec5183a
Axelle <axelle.dup@gmail.com>
2017-07-07 18:25:43 +0200

    cryot

--- cryo_import.R
+++ cryo_import.R
@@ -1,3 +1,13 @@
+library(survival)
+library(gdata)
+library(Hmisc)
+library(compareGroups)
+library(stargazer)
+library(xtable)
+library(dplyr)
+library(ggplot2)
+library(cmprsk)
+
 
 format.pv <- function(p, text=F)
 {
@@ -19,6 +29,18 @@ or.calc <- function(obj, alpha=0.05)
   res <- data.frame(OR=exp(obj$coef), lower=exp(obj$coef-qnorm(1-alpha/2)*se), upper=exp(obj$coef+qnorm(1-alpha/2)*se), p.value=(1-pchisq((obj$coef/se)^2,1)))
   return(res)
 }
+
+
+or.calc2 <- function(obj, alpha=0.05)
+{
+  se <- obj$coefficients[3,2]
+  res <- data.frame(OR=round(exp(obj$coefficients[3,1]),2), lower=round(exp(obj$coefficients[3,1]-qnorm(1-alpha/2)*se),2), upper=round(exp(obj$coefficients[3,1]+qnorm(1-alpha/2)*se),2), p.value=as.numeric(as.character(format.pv(obj$coefficients[3,4]))))
+  return(res)
+}
+
+
+
+
 # nomsvar <- vecnom[i]
 # quali <- quali[i]
 # noms <- valeurs[i]
@@ -36,10 +58,18 @@ disp.logist.u <- function(x, y, dat, res, quali=0, nomvar=NULL, noms=NULL, tex=T
       y <- lrm(formula(paste(y,"~",x,sep="")),data=dat)
       or <- or.calc(y)
       res2 <- rbind(c(nomvar, paste(format.hr(or[2,1])," (",format.hr(or[2,2]),"--", format.hr(or[2,3]), ")",sep=""), format.pv(or[2,4])))
-    }
+   pg<-c("-")
+       res2<-cbind(res2,pg)
+      }
     if(quali==1)
     {
       y <- lrm(formula(paste(y,"~",factor(x),sep="")),data=dat)
+      # tat<-model.matrix(~dat[,x])
+      # colnames(tat)<-c("","tu","su")
+      # dat2<-cbind(dat,tat)
+      # d<-glm(formula(paste(yy,"~",dat2$tu,"+",dat2$su,sep="")),data=dat2,family="binomial")
+      # ano<-anova(d,test="Chisq")
+      
       if(is.null(noms))
       {
         aa<-table(dat[,match(x,names(dat))])
@@ -50,12 +80,16 @@ disp.logist.u <- function(x, y, dat, res, quali=0, nomvar=NULL, noms=NULL, tex=T
       for(i in 2:length(y$coef))
       {
         resi <- c(paste("\\quad ", noms[i], sep=""),paste(format.hr(or[i,1])," (",format.hr(or[i,2]),"--", format.hr(or[i,3]), ")",sep=""), format.pv(or[i,4]))
+        #resi <- c(paste("\\quad ", noms[i], sep=""),paste(format.hr(or[i,1])," (",format.hr(or[i,2]),"--", format.hr(or[i,3]), ")",sep=""), round(ano[i,5],2))
         res2 <- rbind(res2,resi)
       }
+      
+      pg<-c(round(y$stats[5],2),rep("",nrow(res2)-1))
+      res2<-cbind(res2,pg)
     }
     
     
-    colnames(res2) <- c("Variable","OR (95\\%CI)","\\emph{P}")
+    colnames(res2) <- c("Variable","OR (95\\%CI)","\\emph{P}","P")
     return(res2)
   }
   if(tex==FALSE){
@@ -105,21 +139,29 @@ cryo$histof<-as.factor(ifelse(cryo$histo %in% c("CCR"),"CCR","Non-CCR"))
 
 cryo$gradef<-as.factor(cryo$grade)
 cryo$renal_scoref<-as.factor(cryo$renal_score)
-cryo$renal_scorec<-as.factor(ifelse(cryo$renal_score<median(cryo$renal_score),paste0("< ",round(median(cryo$renal_score),0),""),
-                                   paste0(">= ",round(median(cryo$renal_score),0),"")))
+cryo$renal_scorec<-ifelse(cryo$renal_score<7, "simple","intermédiaire")
+cryo$renal_scorec<-ifelse(cryo$renal_score>9, "complexe",cryo$renal_scorec)
+cryo$renal_scorec<-as.factor(cryo$renal_scorec)
 
 
 cryo$mrenal_scoref<-as.factor(cryo$mrenal_score)
 
-cryo$mrenal_scorefc<-as.factor(ifelse(cryo$mrenal_score<median(cryo$mrenal_score),paste0("< ",round(median(cryo$mrenal_score),0),""),
-                                    paste0(">= ",round(median(cryo$mrenal_score),0),"")))
+cryo$mrenal_scorec<-ifelse(cryo$mrenal_score<7, "simple","intermédiaire")
+cryo$mrenal_scorec<-ifelse(cryo$mrenal_score>9, "complexe",cryo$mrenal_scorec)
+cryo$mrenal_scorec<-as.factor(cryo$mrenal_scorec)
+
+
+
+
+
 cryo$volume<-as.numeric(as.character(cryo$volume))
 cryo$volumec<-as.factor(ifelse(cryo$volume<median(cryo$volume),paste0("< ",round(median(cryo$volume),0),""),
                                paste0(">= ",round(median(cryo$volume),0),"")))
 
 cryo$taille_maxc<-as.factor(ifelse(cryo$taille_max<median(cryo$taille_max),paste0("< ",round(median(cryo$taille_max),0),""),
                                paste0(">= ",round(median(cryo$taille_max),0),"")))
-
+cryo$dissectionc<-as.factor(ifelse(cryo$dissection %in% c("0"),"non","oui"))
+table(cryo$dissectionc,exclude=NULL)
 
 cryo$tt_incom<-as.factor(cryo$tt_incom)
 cryo$recidiven<-ifelse(cryo$recidive %in% c("OUI", "OUI "),1,0)
@@ -139,44 +181,81 @@ cryo[cryo$nip %in% e,]
 
 patients_cryo<-cryo[order(cryo$nip,cryo$age),]
 length(unique(cryo$nip)) # 169 patients
-patients_cryo<-patients_cryo %>% distinct(nip, .keep_all = TRUE)
+patients_cryo<-patients_cryo %>% select(everything())%>%  distinct(nip,.keep_all=TRUE)
 
 table(cryo$nip)
-patient_cryo_descri<-TABKRIS(baz=patients_cryo,vect.var = c("age","agec","sex"),
-                                vect.quali = c(0,1,1),
+table(patients_cryo$nip)
+patient_cryo_descri<-TABKRIS(baz=patients_cryo,vect.var = c("age","sex"),
+                                vect.quali = c(0,1),
                                 varint=NULL,valvarint =NULL,
                                 nomvarint = "Histopathologic subtypes",
                                 test=NULL,
-                                vecnoms=c("Age","Age","Sexe"),valeurs=NULL,
+                                vecnoms=c("Age","Sexe"),valeurs=NULL,
                                 vecrefs=NULL,varassoc=NULL,
-                                codassoc=NULL,pres=NULL,langue="en",digits=2)
+                                codassoc=NULL,pres=c("mean_mm",""),langue="en",digits=0)
 
 tumeurs_cryo<-TABKRIS(baz=cryo,vect.var = c("cote","histo","histof","gradef","pole","renal_score",
-                                            "renal_scoref","renal_scorec","mrenal_score","mrenal_scoref","mrenal_scorefc",
+                                            "renal_scoref","renal_scorec","mrenal_score","mrenal_scoref","mrenal_scorec",
                                             "location","renal_sinus","volume"
-                                            ,"volumec","taille_max","taille_maxc","vasc","T"),
-                      vect.quali = c(rep(1,5),0,1,1,0,1,1,1,1,0,1,0,1,1,1),
+                                            ,"taille_max","vasc","T"),
+                      vect.quali = c(rep(1,5),0,1,1,0,1,1,1,1,0,0,1,1),
                       varint=NULL,valvarint =NULL,
                       nomvarint = "Histopathologic subtypes",
                       test=NULL,
-                      vecnoms=c("Cote","Histo","Grade","Pole",
+                      vecnoms=c("Cote","Histo","Histo","Grade","Pole",
                                 "renal score","renal score","renal score","mrenal score","mrenal score","mrenal score",
                                 "location","renal sinus"
-                                ,"volume","volume","taille max","taille max","Hyper ou hypovasculaire
+                                ,"volume","taille max","Hyper ou hypovasculaire
 ","T"),valeurs=NULL,
                       vecrefs=NULL,varassoc=NULL,
-                      codassoc=NULL,pres=NULL,langue="en",digits=2)
+                      codassoc=NULL,pres=c(rep("",5),"mean_mm","","","mean_mm","","","","","mean_mm",
+                                           "mean_mm","","")
+                        ,langue="en",digits=0)
 
 
-tumeurs_cryo_tt<-TABKRIS(baz=cryo,vect.var = c("nbr_a","nbr_af","nbr_afc","dissection","marges","margesc","clavien2","compli"),
-                      vect.quali = c(0,1,1,1,0,1,1,1),
+tumeurs_cryo_tt<-TABKRIS(baz=cryo,vect.var = c("nbr_a","nbr_af","nbr_afc","dissection","marges","margesc","clavien2"),
+                      vect.quali = c(0,1,1,1,0,1,1),
                       varint=NULL,valvarint =NULL,
                       nomvarint = "Histopathologic subtypes",
                       test=NULL,
-                      vecnoms=c("Nombre aiguilles","Nombre aiguilles","Nombre aiguilles","Dissection","Marges","Marges","Clavien","Complications"),valeurs=NULL,
+                      vecnoms=c("Nombre aiguilles","Nombre aiguilles","Nombre aiguilles","Dissection","Marges","Marges","Clavien"),valeurs=NULL,
                       vecrefs=NULL,varassoc=NULL,
              
-                     codassoc=NULL,pres=NULL,langue="en",digits=2)
+                     codassoc=NULL,pres=c("mean_mm",1,1,1,"mean_mm",1,1),langue="en",digits=0)
+
+
+tumeurs_cryo_out<-TABKRIS(baz=cryo,vect.var = c("compli","recidiven"),
+                         vect.quali = c(1,1),
+                         varint=NULL,valvarint =NULL,
+                         nomvarint = "Histopathologic subtypes",
+                         test=NULL,
+                         vecnoms=c("Complications","Recidive"),valeurs=NULL,
+                         vecrefs=NULL,varassoc=NULL,
+                         
+                         codassoc=NULL,pres=NULL,langue="en",digits=0)
+
+
+
+
+biv_patients<-TABKRIS(baz=cryo,vect.var = c("age","agec","sex","cote","histo","histof","gradef","pole","renal_score",
+                                                     "renal_scoref","renal_scorec","mrenal_score","mrenal_scoref",
+                                            "mrenal_scorec",
+                                                     "location","renal_sinus","volume"
+                                                     ,"volumec","taille_max","taille_maxc","vasc","T","nbr_a",
+                                            "nbr_af","nbr_afc","dissection","marges","margesc","clavien2","compli"
+                                            ),
+                             vect.quali = c(0,1,1,rep(1,5),0,1,1,0,1,1,1,1,0,1,0,1,1,1,0,1,1,1,0,1,1,1),
+                             varint=c("recidiven"),valvarint =c("Pas d'echec","Echec"),
+                             nomvarint = "",
+                             test=NULL,
+                             vecnoms=c("Age","Age","Sexe","Cote","Histo","Histo","Grade","Pole",
+                                       "renal score","renal score","renal score","mrenal score","mrenal score","mrenal score",
+                                       "location","renal sinus"
+                                       ,"volume","volume","taille max","taille max","Hyper ou hypovasculaire
+                                ","T","Nombre aiguilles","Nombre aiguilles","Nombre aiguilles","Dissection",
+                                       "Marges","Marges","Clavien","Complication"),valeurs=NULL,
+                             vecrefs=NULL,varassoc=NULL,
+                             codassoc=NULL,pres=NULL,langue="en",digits=2)
 
 
 varlist<-c("agec","sex","nbr_af","cote","histo","gradef","pole",
@@ -190,32 +269,148 @@ capture.output(models, file=paste("C:/Users/adupont/Documents/projetstlouis/cryo
 
 
 
+test<-glm(recidiven~ age, data=cryo,family=binomial )
+1 - pchisq(112.76, 179)
+test2<-glm(recidiven~ factor(age), data=cryo,family=binomial )
+anova(test,test2,test="Chisq")
 
-G<-NULL
-for (i in 1:27){
-  var<-c("age","agec","sex","cote","histof","pole","renal_score",
-         "renal_scoref","renal_scorec","mrenal_score","mrenal_scoref","mrenal_scorefc",
-         "location","renal_sinus","volume"
-         ,"volumec","taille_max","taille_maxc","vasc","T","nbr_a","nbr_af","nbr_afc","dissection","marges","clavien2","compli")[i]
-  t<-c(0,1,1,rep(1,3),0,1,1,0,1,1,1,1,0,1,0,1,1,1,0,1,1,1,0,1,1)[i]
-  nom<-c("Age","Age","Sexe","Cote","Histo","Pole",
-         "renal score","renal score","renal score","mrenal score","mrenal score","mrenal score",
+test<-glm(recidiven~ renal_score, data=cryo,family=binomial )
+1 - pchisq(112.77, 180)
+test2<-glm(recidiven~ factor(age), data=cryo,family=binomial )
+anova(test,test2,test="Chisq")
+
+
+
+
+# b<-c(0.01,0.48,0.71,0.95,1.19,0.01,0.48,1.44,0.71,1.96)
+#  a<-c(127.6,124.0,110.8,103.9,101.5,130.1,122.0,92.3,113.1,83.7)
+#  corrosion<-data.frame(a,b)
+# t<-lm(a~ b, data=corrosion )
+# p<-lm(a~ factor(b), data=corrosion )
+# y<-glm(a~ b, data=corrosion )
+# u<-glm(a~ factor(b), data=corrosion )
+# 
+# anova(y,u,test="F")
+# anova(t,p)
+
+# G<-NULL
+# for (i in 1:24){
+#   var<-c("age","agec","sex","cote","histof","pole","renal_score"
+#          ,"renal_scorec","mrenal_score","mrenal_scorec",
+#          "location","renal_sinus","volume"
+#          ,"volumec","taille_max","taille_maxc","vasc","T","nbr_a","nbr_afc","dissectionc","marges","clavien2","compli")[i]
+#   t<-c(0,1,1,rep(1,3),0,1,0,1,1,1,0,1,0,1,1,1,0,1,1,1,1,1)[i]
+#   nom<-c("Age","Age","Sexe","Cote","Histo","Pole",
+#          "renal score","renal score","mrenal score","mrenal score",
+#          "location","renal sinus"
+#          ,"volume","volume","taille max","taille max","Hyper ou hypovasculaire
+#          ","T","Nombre aiguilles","Nombre aiguilles","Dissection","Marges","Clavien","Complications")[i]
+#   g<-disp.logist.u(y="recidiven",x=var, dat=cryo,quali=t,tex=TRUE,nomvar=nom, noms=NULL)
+# G<-rbind(G,g)
+#   
+#   }
+#   
+# m<-glm(recidiven~ margesc, data=cryo,family=binomial )
+# u<-summary(glm(recidiven~ margesc, data=cryo,family=binomial ))
+# marges<-data.frame(Variable=c("marges","\\quad oui","\\quad non"),OR=
+#                      c("","1", paste(round(exp(u$coefficients)[2,1],2),"(",
+#                                                   round(exp(confint(m))[2,1],3) ,"--", 
+#                                                   
+#                                                   round(exp(confint(m))[2,2],3),")"
+#                                                   )),p=c("","","<0.0001"))     
+#                      
+# colnames(marges)<-colnames(G)
+# G<-rbind(G,marges)
+
+
+# lmm <- lmer(recidiven ~ sex  + (1 | nip), data = cryo,REML = TRUE)
+# 
+# cryo$nip2<-c(1,rep(1:90,2))
+# cryo$recideven5<-c(1,rep(0,95),rep(1,85))
+# glmer(recidiven ~ agec  + sex+(1 | nip2), data = cryo, family = binomial, control = glmerControl(optimizer = "bobyqa"))
+# 
+# glm(recidiven~ sex+agec, data=cryo,family=binomial )
+# 
+
+# "nbr_af","cote","histo","pole",
+# "renal_scoref","mrenal_scoref","location","renal_sinus"
+# ,"volume","taille_max","vasc","T","dissection","margesc","nbr_af","dissection","margesc","clavien2","compli"
+# 
+# 
+# 
+# ,"Nb af","Cote","Histo","Pole",
+# "Renal score","mrenal score","location","Renal sinus"
+# ,"Volume","Taille max","Vascularisation","T","Dissection","Marges","Nbr af","Dissection","Marges","Clavien","Complication"
+
+
+
+G2<-NULL
+for (i in 1:18){
+  var<-c("age","sex","cote","histof","pole","renal_scorec"
+         ,"mrenal_scorec",
+         "location","renal_sinus","volume",
+         "taille_max","vasc","T","nbr_a","nbr_afc","dissectionc","clavien2","compli")[i]
+  t<-c(0,1,1,rep(1,2),1,1,1,1,0,0,1,1,0,1,1,1,1,1)[i]
+  nom<-c("Age","Sexe","Cote","Histo","Pole",
+         "renal score","mrenal score",
          "location","renal sinus"
-         ,"volume","volume","taille max","taille max","Hyper ou hypovasculaire
-         ","T","Nombre aiguilles","Nombre aiguilles","Nombre aiguilles","Dissection","Marges","Clavien","Complications")[i]
+         ,"volume","taille max","Hyper ou hypovasculaire
+         ","T","Nombre aiguilles","Nombre aiguilles","Dissection","Marges","Clavien","Complications")[i]
   g<-disp.logist.u(y="recidiven",x=var, dat=cryo,quali=t,tex=TRUE,nomvar=nom, noms=NULL)
-G<-rbind(G,g)
+  G2<-rbind(G2,g)
   
-  }
-  
+}
+
+m<-glm(recidiven~ margesc, data=cryo,family=binomial )
+u<-summary(glm(recidiven~ margesc, data=cryo,family=binomial ))
+marges<-data.frame(Variable=c("marges","\\quad oui","\\quad non"),OR=
+                     c("","1", paste(round(exp(u$coefficients)[2,1],2),"(",
+                                     round(exp(confint(m))[2,1],3) ,"--", 
+                                     
+                                     round(exp(confint(m))[2,2],3),")"
+                     )),p=c("","","<0.0001"),pp=c("<0.0001","",""))     
+
+colnames(marges)<-colnames(G2)
+G2<-rbind(G2,marges)
 
 
-"nbr_af","cote","histo","pole",
-"renal_scoref","mrenal_scoref","location","renal_sinus"
-,"volume","taille_max","vasc","T","dissection","margesc","nbr_af","dissection","margesc","clavien2","compli"
+colnames(G2)<-c("Variable","OR (95\\%CI)", "\\emph{P}","P maximum vrais" )
+
+sexe<-summary(glm(recidiven~ margesc+sex, data=cryo,family=binomial ))
+cote<-summary(glm(recidiven~ margesc+cote, data=cryo,family=binomial ))
+histo<-summary(glm(recidiven~ margesc+histo, data=cryo,family=binomial ))
+renal<-summary(glm(recidiven~ margesc+renal_sinus, data=cryo,family=binomial ))
+vasc<-summary(glm(recidiven~ margesc+vasc, data=cryo,family=binomial ))
+T<-summary(glm(recidiven~ margesc+T, data=cryo,family=binomial ))
+aig<-summary(glm(recidiven~ margesc+nbr_afc, data=cryo,family=binomial ))
+diss<-summary(glm(recidiven~ margesc+dissection, data=cryo,family=binomial ))
+clavien<-summary(glm(recidiven~ margesc+clavien2, data=cryo,family=binomial ))
+pole<-summary(glm(recidiven~ margesc+pole, data=cryo,family=binomial ))
+
+age<-summary(glm(recidiven~ margesc+age, data=cryo,family=binomial ))
+1-pchisq(112.770 - 19.682, df=2)
 
 
+renal_score<-summary(glm(recidiven~ margesc+renal_score, data=cryo,family=binomial ))
+1-pchisq(112.770 - 19.77, df=2)
 
-,"Nb af","Cote","Histo","Pole",
-"Renal score","mrenal score","location","Renal sinus"
-,"Volume","Taille max","Vascularisation","T","Dissection","Marges","Nbr af","Dissection","Marges","Clavien","Complication"
+
+mrenal_score<-summary(glm(recidiven~ margesc+mrenal_score, data=cryo,family=binomial ))
+1-pchisq(112.770 - 19.77, df=2)
+
+volume<-summary(glm(recidiven~ margesc+volume, data=cryo,family=binomial ))
+1-pchisq(112.770 - 19.77, df=2)
+
+taille<-summary(glm(recidiven~ margesc+taille_max, data=cryo,family=binomial ))
+1-pchisq(112.770 - 19.77, df=2)
+
+
+### 
+cryo$Recurrence<-as.factor(cryo$recidiven)
+levels(cryo$Recurrence)<-c("No recurrence","Recurrence")
+histog<- ggplot(cryo, aes(as.factor(marges),fill=Recurrence))+geom_bar()+
+  scale_fill_manual("legend", values = c("No recurrence" = "#0066CC", "Recurrence" = "red"))+
+xlab("Margins (mm)")+ylab("Number")
+  theme(legend.title=element_blank())
+
+histog
